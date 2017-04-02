b54c1cce6714c3a48dc63dd0da99893e57dbf322
2much4u <christoffwk@gmail.com>
2017-04-02 16:16:19 -0500

    Fixed indentation inconsistencies

--- include/natives.h
+++ include/natives.h
@@ -1,13 +1,13 @@
 /*2much4u*/
 
 static Inline int GET_FRAME_COUNT() {
-    invokeNative(0xFC8202EFC642E6F2);
-    return rets[0];
+	invokeNative(0xFC8202EFC642E6F2);
+	return rets[0];
 }
 
 static Inline Player PLAYER_ID() {
-    invokeNative(0x4F8644AF03D0E0D6);
-    return rets[0];
+	invokeNative(0x4F8644AF03D0E0D6);
+	return rets[0];
 }
 
 static Inline Ped PLAYER_PED_ID() {
@@ -16,32 +16,32 @@ static Inline Ped PLAYER_PED_ID() {
 }
 
 static Inline void SET_SUPER_JUMP_THIS_FRAME(Player player) {
-    args[0] = player;
-    invokeNative(0x57FFF03E423A4C0B);
+	args[0] = player;
+	invokeNative(0x57FFF03E423A4C0B);
 }
 
 static Inline void SET_PLAYER_INVINCIBLE(Player player, BOOL toggle) {
-    args[0] = player;
-    args[1] = toggle;
-    invokeNative(0x239528EACDC3E7DE);
+	args[0] = player;
+	args[1] = toggle;
+	invokeNative(0x239528EACDC3E7DE);
 }
 
 static Inline Blip GET_FIRST_BLIP_INFO_ID(int blipSprite) {
-    args[0] = blipSprite;
-    invokeNative(0x1BEDE233E6CD2A1F);
-    return rets[0];
+	args[0] = blipSprite;
+	invokeNative(0x1BEDE233E6CD2A1F);
+	return rets[0];
 }
 
 static Inline BOOL DOES_BLIP_EXIST(Blip blip) {
-    args[0] = blip;
-    invokeNative(0xA6DB27D19ECBB7DA);
-    return rets[0];
+	args[0] = blip;
+	invokeNative(0xA6DB27D19ECBB7DA);
+	return rets[0];
 }
 
 static Inline struct Vector3 GET_BLIP_COORDS(Blip blip) {
-    args[0] = blip;
-    invokeNative(0x586AFE3FF72D996E);
-    struct Vector3 vector3 = { *(float*)&rets[0], *(float*)&rets[1], *(float*)&rets[2] };
+	args[0] = blip;
+	invokeNative(0x586AFE3FF72D996E);
+	struct Vector3 vector3 = { *(float*)&rets[0], *(float*)&rets[1], *(float*)&rets[2] };
 	return vector3;
 }
 
@@ -101,104 +101,104 @@ static Inline void PLAY_SOUND_FRONTEND(int soundID, char *audioName, char *audio
 }
 
 static Inline void DISPLAY_ONSCREEN_KEYBOARD(int p0, char *windowTitle, char *p2, char *defaultText, char *defaultConcat1, char *defaultConcat2, char *defaultConcat3, int maxInputLength) {
-    args[0] = p0;
-    args[1] = (u64)windowTitle;
-    args[2] = (u64)p2;
-    args[3] = (u64)defaultText;
-    args[4] = (u64)defaultConcat1;
-    args[5] = (u64)defaultConcat2;
-    args[6] = (u64)defaultConcat3;
-    args[7] = maxInputLength;
-    invokeNative(0x00DC833F2568DBF6);
+	args[0] = p0;
+	args[1] = (u64)windowTitle;
+	args[2] = (u64)p2;
+	args[3] = (u64)defaultText;
+	args[4] = (u64)defaultConcat1;
+	args[5] = (u64)defaultConcat2;
+	args[6] = (u64)defaultConcat3;
+	args[7] = maxInputLength;
+	invokeNative(0x00DC833F2568DBF6);
 }
 
 static Inline int UPDATE_ONSCREEN_KEYBOARD() {
-    invokeNative(0x0CF2B696BBF945AE);
-    return rets[0];
+	invokeNative(0x0CF2B696BBF945AE);
+	return rets[0];
 }
 
 static Inline char* GET_ONSCREEN_KEYBOARD_RESULT() {
-    invokeNative(0x8362B09B91893647);
-    return (char*)rets[0];
+	invokeNative(0x8362B09B91893647);
+	return (char*)rets[0];
 }
 
 static Inline Hash GET_HASH_KEY(char* value) {
 	args[0] = (u64)value;
-    invokeNative(0xD24D37CC275948CC);
-    return rets[0];
+	invokeNative(0xD24D37CC275948CC);
+	return rets[0];
 }
 
 static Inline BOOL IS_MODEL_VALID(Hash model) {
 	args[0] = model;
-    invokeNative(0xC0296A2EDF545E92);
-    return rets[0];
+	invokeNative(0xC0296A2EDF545E92);
+	return rets[0];
 }
 
 static Inline BOOL HAS_MODEL_LOADED(Hash model) {
 	args[0] = model;
-    invokeNative(0x98A4EB5D89A0C952);
-    return rets[0];
+	invokeNative(0x98A4EB5D89A0C952);
+	return rets[0];
 }
 
 static Inline void SET_PED_CAN_BE_KNOCKED_OFF_VEHICLE(Ped ped, int state) {
 	args[0] = ped;
 	args[1] = state;
-    invokeNative(0x7A6535691B477C48);
+	invokeNative(0x7A6535691B477C48);
 }
 
 static Inline void SET_ENTITY_INVINCIBLE(Entity entity, BOOL toggle) {
 	args[0] = entity;
 	args[1] = toggle;
-    invokeNative(0x3882114BDE571AD4);
+	invokeNative(0x3882114BDE571AD4);
 }
 
 static Inline void SET_PED_CAN_RAGDOLL(Ped ped, BOOL toggle) {
 	args[0] = ped;
 	args[1] = toggle;
-    invokeNative(0xB128377056A54E2A);
+	invokeNative(0xB128377056A54E2A);
 }
 
 static Inline void CLEAR_PLAYER_WANTED_LEVEL(Player player) {
 	args[0] = player;
-    invokeNative(0xB302540597885499);
+	invokeNative(0xB302540597885499);
 }
 
 static Inline void SET_EXPLOSIVE_AMMO_THIS_FRAME(Player player) {
 	args[0] = player;
-    invokeNative(0xA66C71C98D5F2CFB);
+	invokeNative(0xA66C71C98D5F2CFB);
 }
 
 static Inline void SET_PLAYER_MODEL(Player player, Hash model) {
 	args[0] = player;
 	args[1] = model;
-    invokeNative(0x00A1CADD00108836);
+	invokeNative(0x00A1CADD00108836);
 }
 
 static Inline void SET_PED_DEFAULT_COMPONENT_VARIATION(Ped ped) {
 	args[0] = ped;
-    invokeNative(0x45EEE61580806D63);
+	invokeNative(0x45EEE61580806D63);
 }
 
 static Inline void SET_PED_RANDOM_COMPONENT_VARIATION(Ped ped, BOOL p1) {
 	args[0] = ped;
 	args[1] = p1;
-    invokeNative(0xC8A9481A01E63C28);
+	invokeNative(0xC8A9481A01E63C28);
 }
 
 static Inline void SET_MODEL_AS_NO_LONGER_NEEDED(Hash model) {
 	args[0] = model;
-    invokeNative(0xE532F5D78798DAAB);
+	invokeNative(0xE532F5D78798DAAB);
 }
 
 static Inline void REQUEST_MODEL(Hash model) {
 	args[0] = model;
-    invokeNative(0x963D27A58DF860AC);
+	invokeNative(0x963D27A58DF860AC);
 }
 
 static Inline float GET_ENTITY_HEADING(Entity entity) {
 	args[0] = entity;
-    invokeNative(0xE83D4F9BA2A38914);
-    return *(float*)&rets[0];
+	invokeNative(0xE83D4F9BA2A38914);
+	return *(float*)&rets[0];
 }
 
 static Inline Vehicle CREATE_VEHICLE(Hash modelHash, float x, float y, float z, float heading, BOOL isNetwork, BOOL p6) {
@@ -209,53 +209,53 @@ static Inline Vehicle CREATE_VEHICLE(Hash modelHash, float x, float y, float z, 
 	*(float*)&args[4] = heading;
 	args[5] = isNetwork;
 	args[6] = p6;
-    invokeNative(0xAF35D0D2583051B0);
-    return rets[0];
+	invokeNative(0xAF35D0D2583051B0);
+	return rets[0];
 }
 
 static Inline BOOL DOES_ENTITY_EXIST(Entity entity) {
 	args[0] = entity;
-    invokeNative(0x7239B21A38F536BA);
-    return rets[0];
+	invokeNative(0x7239B21A38F536BA);
+	return rets[0];
 }
 
 static Inline BOOL IS_PED_IN_ANY_VEHICLE(Ped ped, BOOL atGetIn) {
 	args[0] = ped;
 	args[1] = atGetIn;
-    invokeNative(0x997ABD671D25CA0B);
-    return rets[0];
+	invokeNative(0x997ABD671D25CA0B);
+	return rets[0];
 }
 
 static Inline Vehicle GET_VEHICLE_PED_IS_USING(Ped ped) {
 	args[0] = ped;
-    invokeNative(0x6094AD011A2EA87D);
-    return rets[0];
+	invokeNative(0x6094AD011A2EA87D);
+	return rets[0];
 }
 
 static Inline void SET_ENTITY_AS_MISSION_ENTITY(Entity entity, BOOL p1, BOOL byThisScript) {
 	args[0] = entity;
 	args[1] = p1;
 	args[2] = byThisScript;
-    invokeNative(0xAD738C3085FE7E11);
+	invokeNative(0xAD738C3085FE7E11);
 }
 
 static Inline void DELETE_VEHICLE(Vehicle *vehicle) {
 	args[0] = (u64)vehicle;
-    invokeNative(0xEA386986E786A54F);
+	invokeNative(0xEA386986E786A54F);
 }
 
 static Inline void SET_PED_INTO_VEHICLE(Ped ped, Vehicle vehicle, int seatIndex) {
 	args[0] = ped;
 	args[1] = vehicle;
 	args[2] = seatIndex;
-    invokeNative(0xF75B0D629E1C063D);
+	invokeNative(0xF75B0D629E1C063D);
 }
 
 static Inline int GET_RANDOM_INT_IN_RANGE(int startRange, int endRange) {
 	args[0] = startRange;
 	args[1] = endRange;
-    invokeNative(0xD53343AA4FB7DD28);
-    return rets[0];
+	invokeNative(0xD53343AA4FB7DD28);
+	return rets[0];
 }
 
 static Inline void SET_VEHICLE_CUSTOM_PRIMARY_COLOUR(Vehicle vehicle, int r, int g, int b) {
@@ -263,7 +263,7 @@ static Inline void SET_VEHICLE_CUSTOM_PRIMARY_COLOUR(Vehicle vehicle, int r, int
 	args[1] = r;
 	args[2] = g;
 	args[3] = b;
-    invokeNative(0x7141766F91D15BEA);
+	invokeNative(0x7141766F91D15BEA);
 }
 
 static Inline void SET_VEHICLE_CUSTOM_SECONDARY_COLOUR(Vehicle vehicle, int r, int g, int b) {
@@ -271,18 +271,18 @@ static Inline void SET_VEHICLE_CUSTOM_SECONDARY_COLOUR(Vehicle vehicle, int r, i
 	args[1] = r;
 	args[2] = g;
 	args[3] = b;
-    invokeNative(0x36CED73BFED89754);
+	invokeNative(0x36CED73BFED89754);
 }
 
 static Inline void SET_VEHICLE_FIXED(Vehicle vehicle) {
 	args[0] = vehicle;
-    invokeNative(0x115722B1B9C14C1C);
+	invokeNative(0x115722B1B9C14C1C);
 }
 
 static Inline void SET_VEHICLE_FORWARD_SPEED(Vehicle vehicle, float speed) {
 	args[0] = vehicle;
 	*(float*)&args[1] = speed;
-    invokeNative(0xAB54A438726D25D5);
+	invokeNative(0xAB54A438726D25D5);
 }
 
 static Inline void DRAW_RECT(float x, float y, float width, float height, int r, int g, int b, int a) {
@@ -294,7 +294,7 @@ static Inline void DRAW_RECT(float x, float y, float width, float height, int r,
 	args[5] = g;
 	args[6] = b;
 	args[7] = a;
-    invokeNative(0x3A618A217E5154F0);
+	invokeNative(0x3A618A217E5154F0);
 }
 
 static Inline void SET_TEXT_COLOUR(int red, int green, int blue, int alpha) {
@@ -302,13 +302,13 @@ static Inline void SET_TEXT_COLOUR(int red, int green, int blue, int alpha) {
 	args[1] = green;
 	args[2] = blue;
 	args[3] = alpha;
-    invokeNative(0xBE6B23FFA53FB442);
+	invokeNative(0xBE6B23FFA53FB442);
 }
 
 static Inline void SET_TEXT_WRAP(float start, float end) {
 	*(float*)&args[0] = start;
 	*(float*)&args[1] = end;
-    invokeNative(0x63145D9C883A1A70);
+	invokeNative(0x63145D9C883A1A70);
 }
 
 static Inline void SET_TEXT_DROPSHADOW(int distance, int r, int g, int b, int a) {
@@ -317,74 +317,74 @@ static Inline void SET_TEXT_DROPSHADOW(int distance, int r, int g, int b, int a)
 	args[2] = g;
 	args[3] = b;
 	args[4] = a;
-    invokeNative(0x465C84BC39F1C351);
+	invokeNative(0x465C84BC39F1C351);
 }
 
 static Inline void SET_TEXT_OUTLINE() {
-    invokeNative(0x2513DFB0FB8400FE);
+	invokeNative(0x2513DFB0FB8400FE);
 }
 
 static Inline void SET_TEXT_FONT(int fontType) {
 	args[0] = fontType;
-    invokeNative(0x66E0276CC5F6B9DA);
+	invokeNative(0x66E0276CC5F6B9DA);
 }
 
 static Inline void SET_TEXT_CENTRE(BOOL align) {
 	args[0] = align;
-    invokeNative(0xC02F4DBFB51D988B);
+	invokeNative(0xC02F4DBFB51D988B);
 }
 
 static Inline void SET_TEXT_SCALE(float p0, float size) {
 	*(float*)&args[0] = p0;
 	*(float*)&args[1] = size;
-    invokeNative(0x07C837F9A01C34C9);
+	invokeNative(0x07C837F9A01C34C9);
 }
 
 static Inline void BEGIN_TEXT_COMMAND_DISPLAY_TEXT(char *text) {
 	args[0] = (u64)text;
-    invokeNative(0x25FBB336DF1804CB);
+	invokeNative(0x25FBB336DF1804CB);
 }
 
 static Inline void ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(char *text) {
 	args[0] = (u64)text;
-    invokeNative(0x6C188BE134E074AA);
+	invokeNative(0x6C188BE134E074AA);
 }
 
 static Inline void END_TEXT_COMMAND_DISPLAY_TEXT(float x, float y) {
 	*(float*)&args[0] = x;
 	*(float*)&args[1] = y;
-    invokeNative(0xCD015E5BB0D96A57);
+	invokeNative(0xCD015E5BB0D96A57);
 }
 
 static Inline void SET_ENTITY_VISIBLE(Entity entity, BOOL toggle, BOOL unk) {
 	args[0] = entity;
 	args[1] = toggle;
 	args[2] = unk;
-    invokeNative(0xEA1C610A04DB6BBB);
+	invokeNative(0xEA1C610A04DB6BBB);
 }
 
 static Inline void SET_MAX_WANTED_LEVEL(int maxWantedLevel) {
 	args[0] = maxWantedLevel;
-    invokeNative(0xAA5F02DB48D704B9);
+	invokeNative(0xAA5F02DB48D704B9);
 }
 
 static Inline void SET_PLAYER_WANTED_LEVEL(Player player, int wantedLevel, BOOL disableNoMission) {
 	args[0] = player;
 	args[1] = wantedLevel;
 	args[2] = disableNoMission;
-    invokeNative(0x39FF19C64EF7DA5B);
+	invokeNative(0x39FF19C64EF7DA5B);
 }
 
 static Inline void SET_PLAYER_WANTED_LEVEL_NOW(Player player, BOOL p1) {
 	args[0] = player;
 	args[1] = p1;
-    invokeNative(0xE0A7D1E497FFCD6F);
+	invokeNative(0xE0A7D1E497FFCD6F);
 }
 
 static Inline void SET_PED_INFINITE_AMMO_CLIP(Ped ped, BOOL toggle) {
 	args[0] = ped;
 	args[1] = toggle;
-    invokeNative(0x183DADC6AA953186);
+	invokeNative(0x183DADC6AA953186);
 }
 
 static Inline void GIVE_WEAPON_TO_PED(Ped ped, Hash weaponHash, int ammoCount, int isHidden, BOOL equipNow) {
@@ -393,5 +393,5 @@ static Inline void GIVE_WEAPON_TO_PED(Ped ped, Hash weaponHash, int ammoCount, i
 	args[2] = ammoCount;
 	args[3] = isHidden;
 	args[4] = equipNow;
-    invokeNative(0xBF0FD6E56C964FCB);
+	invokeNative(0xBF0FD6E56C964FCB);
 }
